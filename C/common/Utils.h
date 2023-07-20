3674d3c2bf6ba6648c1b5dfbe60c235d853631c2
noran97 <99215076+noran97@users.noreply.github.com>
2023-07-20 15:41:41 +0300

    Update Utils.h

--- C/common/Utils.h
+++ C/common/Utils.h
@@ -5,6 +5,7 @@
 
 #define SET_BIT(_Reg , _BitNum) ( _Reg |=  (1<<_BitNum))
 #define CLR_BIT(_Reg , _BitNum) ( _Reg &= ~(1<<_BitNum))
-
+#define TOG_BIT(_Reg , _BitNum) ( _Reg ^=  (1<<_BitNum))
+#define GET_BIT(_Reg , _BitNum) ( _Reg & (1<<_BitNum)>>_BitNum)
 
 #endif /* _UTILS_H_ */
