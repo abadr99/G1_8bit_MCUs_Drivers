9168526056cbb0988d5f5fa578ef6add11c073c4
noran97 <noranmahmoud597@gmail.com>
2023-07-22 19:39:49 +0200

    adding functions

--- C/mcal/GPIO/GPIO.h
+++ C/mcal/GPIO/GPIO.h
@@ -32,7 +32,11 @@ typedef enum  {
 }direction_t;
 
 
-error_t GPIO_SetPinDirection(port_t port,pin_t pin , direction_t direction);
+error_t GPIO_SetPinDirection(port_t pPortNumber,pin_t pin , direction_t direction);
 error_t GPIO_SetPinValue(port_t PortNumber , pin_t PinNumber , state_t PintVoltageLevel);
+error_t GPIO_GetPinValue(port_t PortNumber , pin_t PinNumber,state_t * state); 
+error_t GPIO_SetPortDirection(port_t PortNumber, uint8_t direction); 
+error_t GPIO_SetPortValue(port_t PortNumber, uint8_t PintVoltageLevel); 
+error_t GPIO_GetPortValue(port_t PortNumber ,uint8_t * state);
 
 #endif /* _GPIO_H_ */
