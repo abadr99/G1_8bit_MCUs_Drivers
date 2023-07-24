930df9f3b3f9203c8be52577c8d1a16cf3afaaf2
ManarAbdelraouf <manar.abdelraouf24@gmail.com>
2023-07-24 22:02:42 +0200

    test

--- C/main.c
+++ C/main.c
@@ -7,7 +7,7 @@ int main() {
     
     GPIO_SetPinDirection(kPORTD , kPIN7 , kOutput); // 0
     while(1) {
-        GPIO_SetPinValue(kPORTD , kPIN7 , kHigh);
+        GPIO_SetPinValue(ORTD , kPIN7 , kHigh);
         _delay_ms(500);
         GPIO_SetPinValue(kPORTD , kPIN7 , kLow);
         _delay_ms(500);
