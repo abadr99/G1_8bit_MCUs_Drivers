3b7a4e3665fd69d977781c2b2043f85fe7197252
nouragamall <nouramohamedgamalll@gmail.com>
2023-07-25 21:47:25 +0300

    add Led interfacing

---
+++ C/hal/LED/LED.c
@@ -0,0 +1,30 @@
+#include "C/common/Types.h"
+#include "C/mcal/GPIO/GPIO.h"
+#include "LED.h"
+
+void LED_Init(LED_t LED_Type)
+{
+    GPIO_SetPinDirection(LED_Type.Port,LED_Type.Pin,kOutput);
+}
+void LED_LedOn(LED_t LED_Type)
+{
+    if(LED_Type.ActiveState == LED_ACTIVE_HIGH)
+    {
+        GPIO_SetPinValue(LED_Type.Port,LED_Type.Pin,kHigh);
+    }
+    else if(LED_Type.ActiveState == LED_ACTIVE_LOW)
+    {
+        GPIO_SetPinValue(LED_Type.Port,LED_Type.Pin,kLow);
+    }
+}
+void LED_LedOff(LED_t LED_Type)
+{
+   if(LED_Type.ActiveState == LED_ACTIVE_HIGH)
+    {
+        GPIO_SetPinValue(LED_Type.Port,LED_Type.Pin,kLow);
+    }
+    else if(LED_Type.ActiveState == LED_ACTIVE_LOW)
+    {
+        GPIO_SetPinValue(LED_Type.Port,LED_Type.Pin,kHigh);
+    } 
+}
---
+++ C/hal/LED/LED.h
@@ -0,0 +1,45 @@
+#ifndef LED_H_
+#define LED_H_
+
+//#include "C/common/Types.h"
+
+typedef enum
+{
+    LED_PORTA,
+    LED_PORTB,
+    LED_PORTC,
+    LED_PORTD,
+    #if MCU_TYPE == _PIC 
+    LED_PORTE,
+    #endif
+}LED_PORT_t;
+
+typedef enum
+{
+    LED_PIN0,
+    LED_PIN1,
+    LED_PIN2,
+    LED_PIN3,
+    LED_PIN4,
+    LED_PIN5,
+    LED_PIN6,
+    LED_PIN7,
+}LED_PIN_t;
+
+typedef enum
+{
+    LED_ACTIVE_LOW,
+    LED_ACTIVE_HIGH,
+}LED_STATE_t;
+
+typedef struct 
+{
+    uint8_t Port;
+    uint8_t Pin;
+    uint8_t ActiveState;
+}LED_t;
+
+void LED_Init(LED_t LED_Type);
+void LED_LedOn(LED_t LED_Type);
+void LED_LedOff(LED_t LED_Type);
+#endif
