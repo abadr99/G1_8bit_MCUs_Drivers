e5c24b00062e3cb79f7bc62bc2c248cf082008d0
nouragamall <nouramohamedgamalll@gmail.com>
2023-07-29 02:04:39 +0300

    add Buzzer Driver at HAL Layer

--- C/main.c
+++ C/main.c
@@ -1,16 +1,16 @@
 #include <util/delay.h>
 #include "common/Types.h"
 #include "common/Utils.h"
-#include "hal/LED/LED.h"
+#include "hal/Buzzer/Buzzer.h"
 int main() {
     
-LED_t LED1={kPORTA,kPIN0,kLedActiveHigh};
-  LED_Init(&LED1);
+Buzzer_t Buzzer1={kPORTA,kPIN0,kBuzzerActiveHigh};
+  Buzzer_Init(&Buzzer1);
   while(1)
   {
-    LED_TurnOn(&LED1);
+    Buzzer_TurnOn(&Buzzer1);
     _delay_ms(500);
-    LED_TurnOff(&LED1);
+    Buzzer_TurnOff(&Buzzer1);
     _delay_ms(500);
   }
 }
