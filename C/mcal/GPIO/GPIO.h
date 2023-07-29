216426a7902035a3a2d05a476bfc3a93678ff2b3
noran97 <noranmahmoud597@gmail.com>
2023-07-29 16:15:58 +0300

    fix issues

--- C/mcal/GPIO/GPIO.h
+++ C/mcal/GPIO/GPIO.h
@@ -15,8 +15,8 @@ typedef enum  {
 error_t GPIO_SetPinDirection(port_t pPortNumber,pin_t pin , direction_t direction);
 error_t GPIO_SetPinValue(port_t PortNumber , pin_t PinNumber , state_t PintVoltageLevel);
 error_t GPIO_GetPinValue(port_t PortNumber , pin_t PinNumber,state_t * state); 
-error_t GPIO_SetPortDirection(port_t PortNumber, uint8_t direction); 
+error_t GPIO_SetPortDirection(port_t PortNumber, uint8_t PortValue); 
 error_t GPIO_SetPortValue(port_t PortNumber, uint8_t PintVoltageLevel); 
-error_t GPIO_GetPortValue(port_t PortNumber ,uint8_t * state);
+error_t GPIO_GetPortValue(port_t PortNumber , uint8_t * state);
 
 #endif /* _GPIO_H_ */
