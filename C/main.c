8631d7a2145677be1070088f78ed226ad16d5f95
AbdelrahmanBadr22 <badr@Ubuntu.myguest.virtualbox.org>
2023-07-31 02:01:17 +0300

    Revert "False test"
    This reverts commit e4463ee190333b5e60e51a44c79462dff715fcee.

--- C/main.c
+++ C/main.c
@@ -2,18 +2,16 @@
 #include "common/Types.h"
 #include "common/Utils.h"
 #include "hal/Buzzer/Buzzer.h"
-
-#define MAX 3
 int main()
 {
 
-  Buzzer_t Buzzer1={kPORTA, kPIN0, kBuzzerActiveHigh};
+Buzzer_t Buzzer1={kPORTA, kPIN0, kBuzzerActiveHigh};
   Buzzer_Init(&Buzzer1);
-  while (1)
-  {
-      Buzzer_TurnOn(&Buzzer1);
-      _delay_ms(500);
-      Buzzer_TurnOff(&Buzzer1);
-      _delay_ms(500);
-  }
+while (1)
+{
+    Buzzer_TurnOn(&Buzzer1);
+    _delay_ms(500);
+    Buzzer_TurnOff(&Buzzer1);
+    _delay_ms(500);
+}
 }
