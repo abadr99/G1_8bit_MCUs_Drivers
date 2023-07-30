0414c909edce5ddb7638fb4226accd37a40d7622
AbdelrahmanBadr22 <badr@Ubuntu.myguest.virtualbox.org>
2023-07-31 00:33:03 +0300

    Remove * from checking

--- scripts/style-checking/rules-scripts/scripts/rules/B005.tcl
+++ scripts/style-checking/rules-scripts/scripts/rules/B005.tcl
@@ -4,12 +4,12 @@
 foreach f [getSourceFileNames] {
     set lineNumber 1
     foreach line [getAllLines $f] {
-        #TODO #29 : Check How can we check divede operand 
-        if [regexp {(\s)(\=|<<|>>|\<\=|\>\=|\=\=|\!\=|\!|\~|\^|\%|\+|\-|\*)(\S)} $line] {
+        #TODO #29 : Check How can we check divede and * operands
+        if [regexp {(\s)(\=|<<|>>|\<\=|\>\=|\=\=|\!\=|\!|\~|\^|\%|\+|\-)(\S)} $line] {
             report $f $lineNumber "Operators should be prefixed and postfixed by space"
-        } elseif [regexp {(\S)(\=|\<\<|>>|\<\=|\>\=|\=\=|\!\=|\!|\~|\^|\%|\+|\-|\*)(\s)} $line] {
+        } elseif [regexp {(\S)(\=|\<\<|>>|\<\=|\>\=|\=\=|\!\=|\!|\~|\^|\%|\+|\-)(\s)} $line] {
             report $f $lineNumber "Operators should be prefixed and postfixed by space"
-        } elseif [regexp {(\S)(\=|\<\<|>>|\<\=|\>\=|\=\=|\!\=|\!|\~|\^|\%|\+|\-|\*)(\S)} $line] {
+        } elseif [regexp {(\S)(\=|\<\<|>>|\<\=|\>\=|\=\=|\!\=|\!|\~|\^|\%|\+|\-)(\S)} $line] {
             report $f $lineNumber "Operators should be prefixed and postfixed by space"
         }
 
