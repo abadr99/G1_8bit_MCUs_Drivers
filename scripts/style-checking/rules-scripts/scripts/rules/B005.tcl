aa38cb22d01dec8522924ee08ca89652a38913ca
AbdelrahmanBadr22 <badr@Ubuntu.myguest.virtualbox.org>
2023-07-31 00:30:27 +0300

    Add B005 rule

---
+++ scripts/style-checking/rules-scripts/scripts/rules/B005.tcl
@@ -0,0 +1,18 @@
+#!/usr/bin/tclsh
+# We should have space between operators e.g. x <= 2
+
+foreach f [getSourceFileNames] {
+    set lineNumber 1
+    foreach line [getAllLines $f] {
+        #TODO #29 : Check How can we check divede operand 
+        if [regexp {(\s)(\=|<<|>>|\<\=|\>\=|\=\=|\!\=|\!|\~|\^|\%|\+|\-|\*)(\S)} $line] {
+            report $f $lineNumber "Operators should be prefixed and postfixed by space"
+        } elseif [regexp {(\S)(\=|\<\<|>>|\<\=|\>\=|\=\=|\!\=|\!|\~|\^|\%|\+|\-|\*)(\s)} $line] {
+            report $f $lineNumber "Operators should be prefixed and postfixed by space"
+        } elseif [regexp {(\S)(\=|\<\<|>>|\<\=|\>\=|\=\=|\!\=|\!|\~|\^|\%|\+|\-|\*)(\S)} $line] {
+            report $f $lineNumber "Operators should be prefixed and postfixed by space"
+        }
+
+        incr lineNumber
+    }
+}
