edb888b3f31234fa9a4a3841862b5a95b3ec2358
nouragamall <nouramohamedgamalll@gmail.com>
2023-07-30 03:02:56 +0300

    fix coding style

--- C/hal/LED/LED.h
+++ C/hal/LED/LED.h
@@ -1,18 +1,16 @@
-#ifndef _LED_H_
-#define _LED_H_
+#ifndef LED_H_
+#define LED_H
 
-typedef enum
-{
+typedef enum {
     kLedActiveLow,
     kLedActiveHigh,
-}LedState_t;
+             }LedState_t;
 
-typedef struct 
-{
+typedef struct {
     port_t Port;
     pin_t Pin;
     LedState_t ActiveState;
-}LED_t;
+               }LED_t;
 
 void LED_Init(LED_t* pLED);
 error_t LED_TurnOn(LED_t* pLED);
--- C/mcal/GPIO/GPIO.c
+++ C/mcal/GPIO/GPIO.c
@@ -82,105 +82,104 @@ error_t GPIO_SetPinValue(port_t PortNumber, pin_t PinNumber,
 		}
 	}else
 	{
-		Ret_ErrorState = kFunctioParameterError; 
+		Ret_ErrorState = kFunctioParameterError;
 	}
-	return Ret_ErrorState; 
+	return Ret_ErrorState;
 }
-error_t GPIO_GetPinValue(port_t PortNumber , pin_t PinNumber,state_t * state) 
-{ 
- 
- error_t Ret_ErrorState = kNoError ; 
-   /* Make sure that the Port number and Pin number are in the valid range */ 
-   if(PortNumber <= GPIO_LAST_REG   && PinNumber <= GPIO_LAST_PIN ) 
-   { 
-    switch(PortNumber) 
-    { 
-      case kPORTA : *state= GET_BIT(GPIOA_IN_REG, PinNumber); break; 
-      case kPORTB : *state= GET_BIT(GPIOB_IN_REG, PinNumber); break; 
-      case kPORTC : *state= GET_BIT(GPIOC_IN_REG, PinNumber); break; 
-      case kPORTD : *state =GET_BIT(GPIOD_IN_REG, PinNumber); break; 
+error_t GPIO_GetPinValue(port_t PortNumber, pin_t PinNumber, state_t * state)
+{
+ error_t Ret_ErrorState = kNoError;
+   /* Make sure that the Port number and Pin number are in the valid range */
+   if (PortNumber <= GPIO_LAST_REG   && PinNumber <= GPIO_LAST_PIN )
+   {
+    switch (PortNumber)
+    {
+      case kPORTA : *state= GET_BIT(GPIOA_IN_REG, PinNumber); break;
+      case kPORTB : *state= GET_BIT(GPIOB_IN_REG, PinNumber); break;
+      case kPORTC : *state= GET_BIT(GPIOC_IN_REG, PinNumber); break;
+      case kPORTD : *state =GET_BIT(GPIOD_IN_REG, PinNumber); break;
 	  #if MCU_TYPE == _PIC
-	  case kPORTE : *state =GET_BIT(GPIO_INPUT_REGE , PinNumber); break; 
+	  case kPORTE : *state =GET_BIT(GPIO_INPUT_REGE, PinNumber); break;
 	  #endif
-    } 
-   } 
-   else 
-   { 
-     Ret_ErrorState = kFunctioParameterError  ; 
-   } 
- 
-    return Ret_ErrorState ; 
- 
+    }
+   }
+   else
+   {
+     Ret_ErrorState = kFunctioParameterError;
+   }
+
+    return Ret_ErrorState;
+
 }
-error_t GPIO_SetPortDirection(port_t PortNumber, uint8_t direction) 
-{ 
- error_t Ret_ErrorState = kNoError ; 
-  /* Make sure that the Port number is in the valid range */ 
-  if(PortNumber <= GPIO_LAST_REG  ) 
-  { 
- 
-    switch(PortNumber) 
-    { 
-     case kPORTA : GPIOA_DIR_REG =direction; break; 
-     case kPORTB : GPIOB_DIR_REG =direction; break; 
-     case kPORTC : GPIOC_DIR_REG =direction; break; 
-     case kPORTD : GPIOD_DIR_REG =direction; break; 
+error_t GPIO_SetPortDirection(port_t PortNumber, uint8_t direction)
+{
+ error_t Ret_ErrorState = kNoError;
+  /* Make sure that the Port number is in the valid range */
+  if (PortNumber <= GPIO_LAST_REG  )
+  {
+
+    switch (PortNumber)
+    {
+     case kPORTA : GPIOA_DIR_REG =direction; break;
+     case kPORTB : GPIOB_DIR_REG =direction; break;
+     case kPORTC : GPIOC_DIR_REG =direction; break;
+     case kPORTD : GPIOD_DIR_REG =direction; break;
 	  #if MCU_TYPE == _PIC
-	  case kPORTE : GPIO_DIRECTION_REGE =direction; break; 
+	  case kPORTE : GPIO_DIRECTION_REGE =direction; break;
 	  #endif
-    } 
- 
-  }else 
-  { 
-   Ret_ErrorState = kFunctioParameterError  ; 
-  } 
- 
-  return Ret_ErrorState ; 
+    }
+
+  }else
+  {
+   Ret_ErrorState = kFunctioParameterError;
+  }
+
+  return Ret_ErrorState;
 }
-error_t GPIO_SetPortValue(port_t PortNumber, uint8_t PintVoltageLevel) 
-{ 
- /* Make sure that the Port number is in the valid range */ 
- error_t Ret_ErrorState = kNoError ; 
- if(PortNumber <= GPIO_LAST_REG  ) 
- { 
-   switch(PortNumber) 
-   { 
-    case kPORTA : GPIOA_OUT_REG =PintVoltageLevel; break; 
-    case kPORTB : GPIOB_OUT_REG =PintVoltageLevel; break; 
-    case kPORTC : GPIOC_OUT_REG =PintVoltageLevel; break; 
-    case kPORTD : GPIOD_OUT_REG =PintVoltageLevel; break; 
+error_t GPIO_SetPortValue(port_t PortNumber, uint8_t PintVoltageLevel)
+{
+ /* Make sure that the Port number is in the valid range */
+ error_t Ret_ErrorState = kNoError;
+ if (PortNumber <= GPIO_LAST_REG  )
+ {
+   switch (PortNumber)
+   {
+    case kPORTA : GPIOA_OUT_REG =PintVoltageLevel; break;
+    case kPORTB : GPIOB_OUT_REG =PintVoltageLevel; break;
+    case kPORTC : GPIOC_OUT_REG =PintVoltageLevel; break;
+    case kPORTD : GPIOD_OUT_REG =PintVoltageLevel; break;
 	#if MCU_TYPE == _PIC
 	case kPORTE : GPIO_DIRECTION_REGE =PintVoltageLevel; break;
 	#endif
-   } 
- 
- }else 
- { 
-  Ret_ErrorState = kFunctioParameterError  ; 
- } 
- return Ret_ErrorState ;  
+   }
+
+ }else
+ {
+  Ret_ErrorState = kFunctioParameterError;
+ }
+ return Ret_ErrorState;
 }
-error_t GPIO_GetPortValue(port_t PortNumber ,uint8_t * state) 
-{ 
- 
- /* Make sure that the Port number is in the valid range */ 
-  error_t Ret_ErrorState = kNoError ; 
-  if(PortNumber <= GPIO_LAST_REG  ) 
-  { 
-    switch(PortNumber) 
-    { 
-     case kPORTA : *state=GPIOA_IN_REG ; break; 
-     case kPORTB : *state=GPIOB_IN_REG ; break; 
-     case kPORTC : *state=GPIOC_IN_REG ; break; 
-     case kPORTD : *state=GPIOD_IN_REG ; break; 
+error_t GPIO_GetPortValue(port_t PortNumber, uint8_t * state)
+{
+
+ /* Make sure that the Port number is in the valid range */
+  error_t Ret_ErrorState = kNoError;
+  if (PortNumber <= GPIO_LAST_REG  )
+  {
+    switch (PortNumber)
+    {
+     case kPORTA : *state=GPIOA_IN_REG; break;
+     case kPORTB : *state=GPIOB_IN_REG; break;
+     case kPORTC : *state=GPIOC_IN_REG; break;
+     case kPORTD : *state=GPIOD_IN_REG; break;
 	 #if MCU_TYPE == _PIC
-	 case kPORTE : *state =GPIO_INPUT_REGE; break; 
+	 case kPORTE : *state =GPIO_INPUT_REGE; break;
 	  #endif
-    } 
- 
-  }else 
-  { 
-   Ret_ErrorState = kFunctioParameterError  ; 
-  } 
-  return Ret_ErrorState ; 
+    }
+
+  }else
+  {
+   Ret_ErrorState = kFunctioParameterError;
+  }
+  return Ret_ErrorState;
 }
