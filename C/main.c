a827a6e828b699b17fde2ca48b61189ef0d0bc82
nouragamall <nouramohamedgamalll@gmail.com>
2023-08-02 15:02:39 +0300

    resolve conflicts

--- C/main.c
+++ C/main.c
@@ -4,14 +4,5 @@
 #include "hal/Relay/Relay.h"
 int main()
 {
-  Relay_t Relay1 = {kPORTA, kPIN0};
-  Relay_Init(&Relay1);
-  while (1)
-  {
-    Relay_TurnOn(&Relay1);
-    //_delay_ms(2000);
-    Relay_TurnOff(&Relay1);
-    //_delay_ms(2000);
-  }
-  
+
 }
