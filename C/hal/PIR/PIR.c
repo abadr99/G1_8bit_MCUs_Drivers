0d5fdd070cca3f3a7ebdb61cd53680ca24df8065
noran97 <noranmahmoud597@gmail.com>
2023-08-09 21:01:33 +0300

    fixing issues

--- C/hal/PIR/PIR.c
+++ C/hal/PIR/PIR.c
@@ -7,12 +7,12 @@ error_t PIR_Init(PIR_t* pPIR)
     error_t retErrorState = kNoError;
     if (pPIR!=NULL)
     {
-    GPIO_SetPinDirection(pPIR->port, pPIR->pin, kInput);
+      GPIO_SetPinDirection(pPIR->port, pPIR->pin, kInput);
     }
     else
- {
-  retErrorState = kFunctionParameterError;
- }
+    {
+        retErrorState = kFunctionParameterError;
+    }
  return retErrorState;
 }
 error_t PIR_GetVal(PIR_t* pPIR, state_t * state)
@@ -20,11 +20,11 @@ error_t PIR_GetVal(PIR_t* pPIR, state_t * state)
     error_t retErrorState = kNoError;
     if (pPIR!=NULL)
     {
-     GPIO_GetPinValue(pPIR->port, pPIR->pin, state);
+        GPIO_GetPinValue(pPIR->port, pPIR->pin, state);
     }
     else
     {
-  retErrorState = kFunctionParameterError;
+        retErrorState = kFunctionParameterError;
     }
- return retErrorState;
+    return retErrorState;
 }
