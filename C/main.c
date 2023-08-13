4b834ec76ec4db84a9bf447eabea85653ae70b12
noran97 <noranmahmoud597@gmail.com>
2023-08-13 17:41:42 +0300

    fix errors

--- C/main.c
+++ C/main.c
@@ -1,11 +1,16 @@
 #include <util/delay.h>
+#include <stdio.h>
 #include "common/Types.h"
 #include "common/Utils.h"
+#include "mcal/GPIO/GPIO.h"
+#include "hal/Keypad/keypadConfig.h"
 #include "hal/Keypad/Keypad.h"
-int main()
-{
-  while (1)
-  {
 
-  }
+
+int main(void)
+{
+while (1)
+{
+
+}
 }
