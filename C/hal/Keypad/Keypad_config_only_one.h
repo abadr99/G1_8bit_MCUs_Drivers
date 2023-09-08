c6521bfbd233954a3b54b30aec783d14b6d549a3
noran97 <noranmahmoud597@gmail.com>
2023-09-08 17:31:36 +0300

    fixing issues

--- C/hal/Keypad/Keypad_config_only_one.h
+++ C/hal/Keypad/Keypad_config_only_one.h
@@ -2,14 +2,9 @@
 #define CONFIG_H_
 
 #define Keypad_numberOfRows (4)
-#define Keypad_numberOfCols (3)
+#define Keypad_numberOfCols (4)
 
 
-uint8_t keypadButtons[Keypad_numberOfRows]
-                     [Keypad_numberOfCols] = {{'1', '2', '3'},
-                                              {'4', '5', '6'},
-                                              {'7', '8', '9'},
-                                              {'*', '0', '#'}
-                                             };
+
 
 #endif
