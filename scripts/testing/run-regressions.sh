5863a4b4e32b9123624bab1ab8eddb0ed03a86a9
AbdelrahmanBadr22 <badr@Ubuntu.myguest.virtualbox.org>
2023-09-27 22:28:02 +0300

    FIX

--- scripts/testing/run-regressions.sh
+++ scripts/testing/run-regressions.sh
@@ -8,7 +8,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 RESET='\033[0m'  # Reset color to default
-
+EXIT=0
 main_C_test_directory=regression-tests/regression-tests
 
 if [ $# != 1 ]
@@ -79,6 +79,7 @@ case $1 in
     ;;
 esac
 rm -rf $test_dir/results.output
-exit 0
+
+#exit $EXIT
 
 
