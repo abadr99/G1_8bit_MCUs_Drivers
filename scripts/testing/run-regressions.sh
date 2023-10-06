3ddbabde3426f8bc7024de5e7e27a9dd9f83d527
AbdelrahmanBadr22 <badr@Ubuntu.myguest.virtualbox.org>
2023-10-06 17:28:15 +0300

    DEBUG

--- scripts/testing/run-regressions.sh
+++ scripts/testing/run-regressions.sh
@@ -10,7 +10,7 @@
 # RESET='\033[0m'  # Reset color to default
 
 main_C_test_directory=regression-tests/regression-tests
-
+exit 1
 if [ $# != 1 ]
 then 
     echo "[run-regression]: Usage run-regression path/to/elf/dir or Usage run-regression all to run all tests"
