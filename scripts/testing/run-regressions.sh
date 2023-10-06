de45e7636b870dd6d6b6df2f1db0311d9934c6af
AbdelrahmanBadr22 <badr@Ubuntu.myguest.virtualbox.org>
2023-10-06 21:16:53 +0300

    Add more info messages

--- scripts/testing/run-regressions.sh
+++ scripts/testing/run-regressions.sh
@@ -10,48 +10,50 @@ GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 Magenta='\033[1;35m'
 RESET='\033[0m'  # Reset color to default
+OK='\033[1;32m [---- OK  ----]:\033[0m'
+OPS='\033[0;31m [--- Ooops ---]:\033[0m'
 
 main_C_test_directory=regression-tests/regression-tests
 
 # -- Check for passed arguments
 if [ $# != 1 ]
 then 
-    echo "[run-regression]: Usage run-regression path/to/elf/dir or Usage run-regression all to run all tests"
+    echo "$RED[run-regression]:$RESET Usage run-regression path/to/elf/dir or Usage run-regression all to run all tests"
     exit 1
 fi
 
 
 if [ -d "$1" ]
 then
-  echo "$GREEN [--- OK ---]:$RESET Directory has been found successfully: $1"
-  test_file=$(find "$1" -type f -name "*.elf" | grep "\.elf$")
+  echo "$OK Directory has been found successfully: $1"
+  test_file=$(find "$1" -type f -name "*.testelf" | grep "\.testelf$")
   if [ -s "$test_file" ]
   then
-    echo "$GREEN [--- OK ---]:$RESET Test-elf file has been found : $test_file" 
+    echo "$OK $RESET Test-elf file has been found : $test_file" 
   else
-    echo "$RED No elf file found in the directory $RESET"
+    echo "$OPS No testelf file found in the directory "
     exit 1
   fi
 else
-  echo "$RED Directory not found $RESET: $directory_to_search"
+  echo "$OPS :$RESET $directory_to_search"
   exit 1 
 fi
 
 test_dir=$(dirname "$test_file")
-echo "$YELLOW Testing: $test_file $RESET" 
+echo "$YELLOW [-- TESTING --]: $test_file $RESET" 
 
 if [ -s "$test_dir/results.expected" ]
 then
-  echo "$GREEN [--- OK ---]:$RESET Expected results file have been found : $test_file"
+  echo "$OK Expected results file have been found : $test_file"
 else
-  echo "$RED No results.expected file found in the directory$RESET: $test_dir"
+  echo "$OPS No results.expected file found in the directory: $test_dir"
   exit 1
 fi
 
 simulavr -d atmega32 -f $test_file -W 0x20,- -R 0x22,- -T exit > $test_dir/results.output
 if [ -s "$test_dir/results.output" ]
 then
-  echo "$GREEN [--- OK ---]:$RESET Output file has been generated successfully : $test_file"
+  echo "$OK Output file has been generated successfully : $test_file"
 fi
 
 diff $test_dir/results.output $test_dir/results.expected
