d049d8c6c8971326cf952a55678bae390a608d68
AbdelrahmanBadr22 <badr@Ubuntu.myguest.virtualbox.org>
2023-10-07 02:33:07 +0300

    Add new type

--- C/common/Types.h
+++ C/common/Types.h
@@ -7,6 +7,11 @@ typedef float f32_t;                            //IGNORE-STYLE-CHECK[B004]
 typedef unsigned short int  uint16;
 typedef signed short int    sint16;
 
+typedef unsigned long uint32_t;                  //IGNORE-STYLE-CHECK[B004]
+typedef signed   long sint32_t;                  //IGNORE-STYLE-CHECK[B004]
+
+
+
 #define NULL_PTR ((void*)0)
 
 #ifndef NULL
