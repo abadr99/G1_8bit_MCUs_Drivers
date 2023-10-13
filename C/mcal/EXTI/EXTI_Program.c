9a3151df4076bc1da48d5baf557da85b5c0e5381
Abdelrahman Badr <abdelrhmanezzbadr@outlook.com>
2023-10-14 01:08:23 +0300

    Revert "add Interrupt for pic microcontroller[Noura,Manar]"

--- C/mcal/EXTI/EXTI_Program.c
+++ C/mcal/EXTI/EXTI_Program.c
@@ -11,7 +11,9 @@
 #include "../../common/Registes.h"
 #include "EXTI_Private.h"
 #include "EXTI_Interface.h"
-#include "../ISR/ISR.h"
+
+
+void (*EXTI_pCallBackFun[3]) (void) = {NULL_PTR};
 
 error_t EXTI_InterruptEnable (uint8_t kInterruptSource)
 {
@@ -26,17 +28,6 @@ error_t EXTI_InterruptEnable (uint8_t kInterruptSource)
         default: kErrorState = kFunctionParameterError;
     }
     #endif
-    #if MCU_TYPE == _PIC
-    switch (kInterruptSource)
-    {
-        case EXTI_INT0 : SET_BIT(INTCON_REG, INTCON_INT0E);    break;
-        case EXTI_INT1 : SET_BIT(INTCON3_REG, INTCON3_INT1IE); break;
-        case EXTI_INT2 : SET_BIT(INTCON3_REG, INTCON3_INT2IE); break;
-        case EXTI_RB   : SET_BIT(INTCON_REG, INTCON_RBIE);     break;
-        default: kErrorState = kFunctionParameterError;        break;
-    }
-    #endif
-
     return kErrorState;
 }
 error_t EXTI_InterruptDisable(uint8_t kInterruptSource)
@@ -50,16 +41,6 @@ error_t EXTI_InterruptDisable(uint8_t kInterruptSource)
         case EXTI_INT2 : CLR_BIT(GICR_REG, GICR_INT2); break;
 
         default: kErrorState = kFunctionParameterError;
-    }
-    #endif
-    #if MCU_TYPE == _PIC
-    switch (kInterruptSource)
-    {
-        case EXTI_INT0 : CLR_BIT(INTCON_REG, INTCON_INT0E);    break;
-        case EXTI_INT1 : CLR_BIT(INTCON3_REG, INTCON3_INT1IE); break;
-        case EXTI_INT2 : CLR_BIT(INTCON3_REG, INTCON3_INT2IE); break;
-        case EXTI_RB   : SET_BIT(INTCON_REG, INTCON_RBIE);     break;
-        default: kErrorState = kFunctionParameterError;        break;
     }
     #endif
     return kErrorState;
@@ -135,101 +116,19 @@ error_t EXTI_SetSenseControl (uint8_t kInterruptSource, uint8_t kSenseControl)
         kErrorState = kFunctionParameterError;
     }
     #endif
-    #if MCU_TYPE == _PIC
-    if (kInterruptSource == EXTI_INT0)
-    {
-        switch (kSenseControl)
-        {
-            case EXTI_FALLING_EDGE : CLR_BIT(INTCON2_REG,
-                                             INTCON2_INTEDG0);
-                                             break;
-            case EXTI_RISING_EDGE  : SET_BIT(INTCON2_REG,
-                                             INTCON2_INTEDG0);
-                                             break;
-            default: kErrorState = kFunctionParameterError;
-                                             break;
-        }
-    }
-    else if (kInterruptSource == EXTI_INT1)
-    {
-        switch (kSenseControl)
-        {
-            case EXTI_FALLING_EDGE : CLR_BIT(INTCON2_REG,
-                                             INTCON2_INTEDG1);
-                                             break;
-            case EXTI_RISING_EDGE  : SET_BIT(INTCON2_REG,
-                                             INTCON2_INTEDG1);
-                                             break;
-            default: kErrorState = kFunctionParameterError;
-                                             break;
-        }
-    }
-    else if (kInterruptSource == EXTI_INT2)
-    {
-        switch (kSenseControl)
-        {
-            case EXTI_FALLING_EDGE : CLR_BIT(INTCON2_REG,
-                                             INTCON2_INTEDG2);
-                                             break;
-            case EXTI_RISING_EDGE  : SET_BIT(INTCON2_REG,
-                                             INTCON2_INTEDG2);
-                                             break;
-            default: kErrorState = kFunctionParameterError;
-                                             break;
-        }
-    }
-
-    #endif
-
     return kErrorState;
 }
-#if MCU_TYPE == _PIC
-error_t EXTI_ClearFlag(uint8_t kInterruptSource)
-{
-    error_t kErrorState = kNoError;
-    switch (kInterruptSource)
-    {
-    case EXTI_INT0 : CLR_BIT(INTCON_REG, INTCON_INT0F);     break;
-    case EXTI_INT1 : CLR_BIT(INTCON3_REG, INTCON3_INT1IF);  break;
-    case EXTI_INT2 : CLR_BIT(INTCON3_REG, INTCON3_INT2IF);  break;
-    default: kErrorState = kFunctionParameterError;         break;
-    }
-    return kErrorState;
-}
-error_t EXTI_SetPriority(uint8_t kInterruptSource, uint8_t kInterruptPriority)
-{
-    error_t kErrorState = kNoError;
-    if (kInterruptPriority == EXIT_HIGH_PRIORITY)
-    {
-        switch (kInterruptSource)
-        {
-        case EXTI_INT1 : SET_BIT(INTCON3_REG, INTCON3_INT1IP);  break;
-        case EXTI_INT2 : SET_BIT(INTCON3_REG, INTCON3_INT2IP);  break;
-        default: kErrorState = kFunctionParameterError;         break;
-        }
-    }
-    else if (kInterruptPriority == EXIT_LOW_PRIORITY)
-    {
-        switch (kInterruptSource)
-        {
-        case EXTI_INT1 : CLR_BIT(INTCON3_REG, INTCON3_INT1IP);  break;
-        case EXTI_INT2 : CLR_BIT(INTCON3_REG, INTCON3_INT2IP);  break;
-        default: kErrorState = kFunctionParameterError;         break;
-        }
-    }
-    return kErrorState;
-}
-#endif
 error_t EXTI_SetCallBackFun  (uint8_t kInterruptSource, void (*pFun)(void))
 {
     error_t kErrorState = kNoError;
+    #if MCU_TYPE ==_AVR
     if (pFun != NULL_PTR)
     {
         switch (kInterruptSource)
         {
-            case EXTI_INT0 : ISR_Init(EXTI_INT0, pFun); break;
-            case EXTI_INT1 : ISR_Init(EXTI_INT1, pFun); break;
-            case EXTI_INT2 : ISR_Init(EXTI_INT2, pFun); break;
+            case EXTI_INT0 : EXTI_pCallBackFun[0] = pFun; break;
+            case EXTI_INT1 : EXTI_pCallBackFun[1] = pFun; break;
+            case EXTI_INT2 : EXTI_pCallBackFun[2] = pFun; break;
 
             default: kErrorState = kFunctionParameterError;
         }
@@ -237,5 +136,31 @@ error_t EXTI_SetCallBackFun  (uint8_t kInterruptSource, void (*pFun)(void))
     {
         kErrorState = kFunctionParameterError;
     }
+    #endif
     return kErrorState;
+}
+/* ISR Implementation   */
+void __vector_1(void) __attribute__((signal));
+void __vector_1(void)
+{
+    if (EXTI_pCallBackFun[0]!=NULL_PTR)
+    {
+        EXTI_pCallBackFun[0]();
+    }
+}
+void __vector_2(void) __attribute__((signal));
+void __vector_2(void)
+{
+    if (EXTI_pCallBackFun[1]!=NULL_PTR)
+    {
+        EXTI_pCallBackFun[1]();
+    }
+}
+void __vector_3(void) __attribute__((signal));
+void __vector_3(void)
+{
+    if (EXTI_pCallBackFun[2]!=NULL_PTR)
+    {
+        EXTI_pCallBackFun[2]();
+    }
 }
