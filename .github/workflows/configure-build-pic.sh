90a3d39a78616179aa15fd6514c404d0a3dcfc62
AbdelrahmanBadr22 <badr@Ubuntu.myguest.virtualbox.org>
2023-10-20 19:21:49 +0300

    DBG CI

--- .github/workflows/configure-build-pic.sh
+++ .github/workflows/configure-build-pic.sh
@@ -1,8 +1,8 @@
 wget -O xc8_installer.run https://ww1.microchip.com/downloads/aemDocuments/documents/DEV/ProductDocuments/SoftwareTools/xc8-v2.45-full-install-linux-x64-installer.run
 chmod +x xc8_installer.run
-
-sudo visudo
 user=$(whoami)
-
-$user ALL=(ALL) NOPASSWD: xc8_installer.run --mode unattended --unattendedmodeui none --netservername some-placeholder-value | sudo tee -a /etc/sudoers
+sudo -i
+cp /etc/sudoers /root/sudoers.bak
+visudo
+echo "$user ALL=(ALL) NOPASSWD: xc8_installer.run --mode unattended --unattendedmodeui none --netservername some-placeholder-value" >>  /etc/sudoers
 sudo xc8_installer.run --mode unattended --unattendedmodeui none --netservername some-placeholder-value
