71f8de582085b543752c63829ccecfb0e2c6371e
AbdelrahmanBadr22 <badr@Ubuntu.myguest.virtualbox.org>
2023-10-20 19:41:35 +0300

    DBG CI

--- .github/workflows/configure-build-pic.sh
+++ .github/workflows/configure-build-pic.sh
@@ -1,7 +1,6 @@
-wget -O xc8_installer.run https://ww1.microchip.com/downloads/aemDocuments/documents/DEV/ProductDocuments/SoftwareTools/xc8-v2.45-full-install-linux-x64-installer.run
+wget -O xc8-v1.20-linux.run https://ww1.microchip.com/downloads/aemDocuments/documents/DEV/ProductDocuments/SoftwareTools/xc8-v2.45-full-install-linux-x64-installer.run
 #chmod +x xc8_installer.run
 	
-tar -xvf xc8-v1.20-linux.tar
 chmod +x xc8-v1.20-linux.run
 sudo ./xc8-v1.20-linux.run
 
