e95f2bad538bc6f221b59f31f700458a8d69f404
AbdelrahmanBadr22 <badr@Ubuntu.myguest.virtualbox.org>
2023-10-20 20:46:28 +0300

    DBG CI

--- .github/workflows/configure-build-pic.sh
+++ .github/workflows/configure-build-pic.sh
@@ -1,4 +1,5 @@
 wget -O xc8-v2.45-linux.run https://ww1.microchip.com/downloads/aemDocuments/documents/DEV/ProductDocuments/SoftwareTools/xc8-v2.45-full-install-linux-x64-installer.run
 chmod +x xc8-v2.45-linux.run
 sudo ./xc8-v2.45-linux.run --mode unattended --unattendedmodeui none --netservername localhost
+ls /opt
 export PATH="/opt/microchip/xc8/v2.45/bin/:$PATH"
