e35e1128b0e61729a0e7fe5e059bfa2637e75b60
Noura Gamal <116299689+nouragamall@users.noreply.github.com>
2023-10-27 19:59:49 +0200

    add Calculator app[@nouragamall,@AbdelrahmanBadr22] (#88)
    Co-authored-by: nouragamall <nouramohamedgamalll@gmail.com>
    Co-authored-by: Abdelrahman Badr <abdelrhmanezzbadr@outlook.com>

---
+++ C/app/Calculator/Noura_Gamal/main.c
@@ -0,0 +1,27 @@
+#include <util/delay.h>
+#include "../../../common/Types.h"
+#include "../../../common/Utils.h"
+#include "../../../utils/stack/stack.h"
+#include "../../../hal/Keypad/Keypad_config_only_one.h"
+#include "../../../hal/Keypad/Keypad_only_one.h"
+#include "../../../hal/LCD/LCD_Interface.h"
+#include "calc.h"
+int main()
+{
+    keypad_t keypad;
+    lcd_t lcd;
+    CalcConf(&keypad, &lcd);
+    uint8_t key =0;
+    charStack_t opStack;
+    charStack_Init(&opStack);
+    sint32_tStack_t numStack;
+    sint32_tStack_Init(&numStack);
+    while (1)
+    {
+        do
+        {
+            key = Keypad_GetPressedButton(&keypad);
+        } while (key == NOT_PRESSED);
+        Calculator(&opStack, &numStack, &lcd, key);
+    }
+}
