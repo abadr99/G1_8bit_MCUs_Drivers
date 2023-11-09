0f05f45c6193dd9d027005b1a52c9bf7b5a37d1f
nouragamall <nouramohamedgamalll@gmail.com>
2023-11-10 01:15:26 +0200

    some changes

--- C/mcal/UART/UART.c
+++ C/mcal/UART/UART.c
@@ -8,6 +8,7 @@
 error_t UART_Init(void)
 {
     error_t kErrorState = kNoError;
+    #if MCU_TYPE == _PIC
     /*Asynchronous mode*/
     SET_BIT(RCSTA_REG, RCSTA_SPEN);
     CLR_BIT(TXSTA_REG, TXSTA_SYNC);
@@ -20,21 +21,25 @@ error_t UART_Init(void)
     SET_BIT(TXSTA_REG, TXSTA_TXEN);
     /*Receiver Enable*/
     SET_BIT(RCSTA_REG, RCSTA_CREN);
+    #endif
     return kErrorState;
 }
 error_t UART_Transmit(uint8_t data)
 {
+    #if MCU_TYPE == _PIC
     error_t kErrorState = kNoError;
     while ((GET_BIT(TXSTA_REG, TXSTA_TRMT)) == 0)
     {
 
     }
     TXREG_REG = data;
+    #endif
     return kErrorState;
 }
 error_t UART_Receive(uint8_t *data)
 {
     error_t kErrorState = kNoError;
+    #if MCU_TYPE == _PIC
     if (data != NULL)
     {
         while ((GET_BIT(PIR1_REG, PIR1_RCIF)) == 0)
@@ -47,8 +52,10 @@ error_t UART_Receive(uint8_t *data)
     {
         kErrorState = kFunctionParameterError;
     }
+    #endif
     return kErrorState;
 }
+#if MCU_TYPE == _PIC
 error_t ClearFlag(void)
 {
     error_t kErrorState = kNoError;
@@ -58,3 +65,4 @@ error_t ClearFlag(void)
     }
     return kErrorState;
 }
+#endif
