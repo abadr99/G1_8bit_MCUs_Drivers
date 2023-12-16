e32eece31ca0112ebb85cbc0219d3fa69c99fe1d
Mariamahmed44 <115891752+Mariamahmed44@users.noreply.github.com>
2023-12-17 01:20:23 +0200

    adding a function that prints the binary of a number (#119)

--- C/common/test.h
+++ C/common/test.h
@@ -35,6 +35,29 @@ void print_num(int num)
     }
 
 }
+void print_num_binary(int num)
+{
+    int str[sizeof(int)*8];
+    int i = 0;
+    if (num==0)
+    {
+    print_str("0b");
+    SPECIAL_OUTPUT_PORT ='0';
+    }
+    else
+    {
+    for (i=0;num>0;i++)
+    {
+    str[i]=num%2;
+    num=num/2;
+    }
+    print_str("0b");
+    for (i=i-1;i>=0;i--)
+    {
+    SPECIAL_OUTPUT_PORT = str[i]+'0';
+    }
+    }
+}
 
 void print_char(char C)
 {
